e12c1f15430f98f5334fa0c4e53ab7d663848e67
Stefan Dorresteijn <stefan@refreshseo.nl>
2020-10-26 15:36:10 +0100

    Add a "logo"

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,11 @@
         <div class="flex justify-between h-16">
           <div class="flex">
             <div class="flex-shrink-0 flex items-center">
-              <p>Ports</p>
+              <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
+                </svg>
+              </div>
             </div>
             <div class="hidden sm:-my-px sm:ml-6 space-x-8 sm:flex">
               <router-link to="/" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">Home</router-link>
